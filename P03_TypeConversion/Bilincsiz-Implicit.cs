a1d2d00c24331f9640d4ef1645509d215dd7201d
ukaracivio <umit.karacivi@outlook.com>
2024-01-08 19:58:03 +0300

    Some changes

---
+++ P03_TypeConversion/Bilincsiz-Implicit.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P03_TypeConversion
+{
+    // Yani burada sadece implicit örnekleri bulunacak
+
+    internal class Bilincsiz_Implicit
+    {
+        // burası da bu classın giriş noktası
+
+        public static void Main(string[] args)
+        {
+            // Bilinçsiz tür dönüşümleri
+
+            byte a = 10;
+
+            short b = 20;
+
+            sbyte c = 30;
+
+            // 3 farklı veri yapısında değişler tanımlandı.
+
+            int d = a + b + c; // küçük vt --> buyuk vt oto geçiş
+
+            string e = "ARI Bilgi Project";
+
+            char f= 'A'; // char da tek tırnak
+
+            object g = e + " " + f + " " + d; // string char ve int birleştirmesi yapıldı.
+
+            long h = d; // int -> long
+
+            float i = h; // long-> float
+
+            double j = i; // float -> double
+
+            double k = 21.5; // decimal -> double
+
+            Console.WriteLine("1.durum (d) : " + d.ToString());
+
+            Console.WriteLine("2.durum (g) : " + g.ToString());
+            double sonuc = h + k;
+
+            Console.WriteLine("3.durum (h ve k) : " + sonuc);
+
+
+
+
+
+
+        }
+    }
+}
--- P03_TypeConversion/Program.cs
+++ P03_TypeConversion/Program.cs
@@ -1,7 +1,28 @@
-﻿internal class Program
+﻿using P03_TypeConversion;
+
+internal class Program
 {
+    // Bu proje Data Type Conversion (çevrimleri) üzerinedir.
+    // İncelenecek konular
+    // 2 tür çevrim bulunmakta
+
+    // 1.Bilinçsiz - Implicit (küçük veriden büyük veriye kolayca geçiş)
+    // 2.Bilinçli - Explicit 
+
+    // Yapıyı bölerek kontrol eden program parçası burası olacak (yani Program.cs isimli class (sınıf)
+
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // ilk önce Program.cs-Main çalışacak.
+        // Fakat benim istediğim Bilincsiz-Implicit.cs kısmındaki Main bloğu
+        // onu ayağa kaldırmak için 
+        // <class>.<metod> yapısı syntax
+
+        Bilincsiz_Implicit.Main(args);
+
+
+
+
+        Console.ReadKey(); // ekranda bekler
     }
 }
