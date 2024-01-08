2c67ba4eeeca6171107a13c16fa5b5fcf877414f
ukaracivio <umit.karacivi@outlook.com>
2024-01-08 21:33:39 +0300

    Some changes

--- P04_Operators/ArtirmaEksiltme.cs
+++ P04_Operators/ArtirmaEksiltme.cs
@@ -27,7 +27,7 @@ namespace P04_Operators
 
             Console.WriteLine("İşlem öncesi fonceki değeri " + fonceki );
 
-            int fsonraki = ++fonceki; // prefix kullanım
+            int fsonraki = --fonceki; // prefix kullanım
             // ++ operatoru hem fonceki değerinini 1 artırıyor ...sonra bu değeri tutuyor kafada ve en son olarak da fsonraki değişgenine atıyor..
 
             Console.WriteLine("İşlemden sonraki fonceki değeri " + fonceki + ", fsonraki değeri " + fsonraki + "\n\n");
@@ -36,11 +36,27 @@ namespace P04_Operators
 
             Console.WriteLine("İşlem öncesi gonceki değeri " + gonceki);
 
-            int gsonraki = gonceki++; // suffix kullanım
+            int gsonraki = gonceki--; // suffix kullanım
             // ++ operatoru önce gsonraki <- gonceki içeriği atanıyor. sonra değerini 1 artırıyor
 
             Console.WriteLine("İşlemden sonraki gonceki değeri " + gonceki + ", gsonraki değeri " + gsonraki + "\n\n");
 
+
+            // Karışık gibi olabili mi
+
+            int h = 5;
+
+            int i = h++; // i=6;
+
+            int j = i--; // j=5
+
+            int k = 10;
+
+            int l = --k; // l=9
+
+            // değişgen değerlerini yazdırırken parametrik yapı kullanılabilir. CW nin biraz değişik şekli...
+
+            Console.WriteLine("Sonuç : h={0}\ti={1}\tj={2}\tk={3}\tl{4}\n\n",h,i,j,k,l);
 
 
 
