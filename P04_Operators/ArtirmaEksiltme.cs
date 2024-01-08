ecb0dcf8f1cc3c3b3b21c457e08a79f446310361
ukaracivio <umit.karacivi@outlook.com>
2024-01-08 21:07:05 +0300

    Some changes

---
+++ P04_Operators/ArtirmaEksiltme.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P04_Operators
+{
+
+    internal class ArtirmaEksiltme
+    {
+        public static void Main(string[] args)
+        {
+            Console.Clear(); // ekranı temizler
+
+            Console.WriteLine("******************************************");
+            Console.WriteLine("*                                        *");
+            Console.WriteLine("* ARTIRMA VE EKSİLTME OPERATÖR ÖRNEKLERİ *");
+            Console.WriteLine("*                                        *");
+            Console.WriteLine("******************************************");
+
+            // Bir arttırma/eksiltme operatör ornekleri
+            // Çok kullanılır..Sabitlere(const) uygulanmazlar
+            // ön ek ve son ek (prefix-suffix) olmak üzere 2 sekilde kullanılırlar.
+
+            int fonceki = 5;
+
+            Console.WriteLine("İşlem öncesi fonceki değeri " + fonceki );
+
+            int fsonraki = ++fonceki; // prefix kullanım
+            // ++ operatoru hem fonceki değerinini 1 artırıyor ...sonra bu değeri tutuyor kafada ve en son olarak da fsonraki değişgenine atıyor..
+
+            Console.WriteLine("İşlemden sonraki fonceki değeri " + fonceki + ", fsonraki değeri " + fsonraki + "\n\n");
+
+            int gonceki = 10;
+
+            Console.WriteLine("İşlem öncesi gonceki değeri " + gonceki);
+
+            int gsonraki = gonceki++; // suffix kullanım
+            // ++ operatoru önce gsonraki <- gonceki içeriği atanıyor. sonra değerini 1 artırıyor
+
+            Console.WriteLine("İşlemden sonraki gonceki değeri " + gonceki + ", gsonraki değeri " + gsonraki + "\n\n");
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ P04_Operators/Program.cs
@@ -0,0 +1,11 @@
+﻿using P04_Operators;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ArtirmaEksiltme.Main(args);
+
+        Console.ReadKey();
+    }
+}
