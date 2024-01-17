13e6d6988c07bdada0b7dd21f0612a01869c3ea2
ukaracivio <umit.karacivi@outlook.com>
2024-01-17 21:03:47 +0300

    Some changes

--- P05_FlowMechanims/While.cs
+++ P05_FlowMechanims/While.cs
@@ -21,18 +21,71 @@ namespace P05_FlowMechanims
             #region while 1
             // 1 den 100 e kadar sayıların toplamı
 
-            int toplam = 0;
+            //int toplam = 0;
 
-            int sayi = 0;
+            //int sayi = 0;
 
-            while (sayi < 100) // direkt olarak kontrol..doğruysa bloğu çalıştır.
+            //while (sayi < 100) // direkt olarak kontrol..doğruysa bloğu çalıştır.
+            //{
+            //    toplam += sayi;
+
+            //    sayi++; // bir sonraki sayıya geçişi kendim sağlamış oldum.
+            //}
+
+            //Console.WriteLine($"Toplam = {toplam}");
+
+
+            #endregion
+
+
+            #region while 2
+            // Ekrandan girilen bir sayının basamaklarını bulma
+            //int sayi; // ekrandan girilecek olan sayı
+            //int basamak = 0;
+
+            //Console.WriteLine("Lütfen sayınızı giriniz...");
+            //sayi=Convert.ToInt32(Console.ReadLine());
+
+            //while(sayi > 0)
+            //{
+            //    basamak++;
+
+            //    sayi = sayi / 10;
+
+            //}
+
+            //Console.WriteLine("Girdiğiniz sayı " + basamak + " basamaklıdır..."  );
+
+            #endregion
+
+
+            #region while 3 (nested)
+            // devamlı sekilde...nested
+
+            Console.Write("Bir sayı giriniz : ");
+            int sayi=Convert.ToInt32(Console.ReadLine()) ;
+
+            int index = 1;
+
+            // İç içe while döngüleri
+            while (index <= sayi) // olduğu sürece
             {
-                toplam += sayi;
+                Console.WriteLine();
 
-                sayi++; // bir sonraki sayıya geçişi kendim sağlamış oldum.
+                int j = 1;
+
+                while (j <= index) 
+                {
+                    Console.Write("* ");
+                    j++;
+                }
+
+                index++;
             }
 
-            Console.WriteLine($"Toplam = {toplam}");
+
+
+
 
 
             #endregion
