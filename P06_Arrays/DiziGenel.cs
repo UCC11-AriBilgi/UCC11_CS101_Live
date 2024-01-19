30d2130cc7617e95898f9b2253f4505395098a39
ukaracivio <umit.karacivi@outlook.com>
2024-01-19 19:45:35 +0300

    Some changes

---
+++ P06_Arrays/DiziGenel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P06_Arrays
+{
+    internal class DiziGenel
+    {
+        public static void Main(string[] args)
+        {
+
+
+            #region Dizi tanımlama, değer atama ve kullanma
+
+            // 20 elemanlı ..adı dizi olan ve tamsayı içerecek
+
+            int[] dizi = new int[20]; // 20 elemanlı dizi tanımlandı.
+
+            dizi[5]= 30; // dizi ismindeki dizinin 5.indexine 30 sayısını atıyorum...
+
+            Console.WriteLine("Dizinin elemanı : " + dizi[5]); // ekrana yazdırıyorum...
+
+            #endregion
+
+
+        }
+    }
+}
---
+++ P06_Arrays/Program.cs
@@ -0,0 +1,23 @@
+﻿using P06_Arrays;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Clear(); // ekranı temizler
+
+        Console.WriteLine("******************************************");
+        Console.WriteLine("*                                        *");
+        Console.WriteLine("* DİZİ ÖRNEKLERİ                         *");
+        Console.WriteLine("*                                        *");
+        Console.WriteLine("******************************************\n\n\n");
+
+
+
+        DiziGenel.Main(args);
+
+
+
+        Console.ReadKey();
+    }
+}
