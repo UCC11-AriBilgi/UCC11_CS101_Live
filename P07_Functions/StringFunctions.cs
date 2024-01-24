194341cdeabb3e625b6e522fdd78c656790906af
ukaracivio <umit.karacivi@outlook.com>
2024-01-24 19:24:55 +0300

    Some changes

--- P07_Functions/StringFunctions.cs
+++ P07_Functions/StringFunctions.cs
@@ -10,9 +10,43 @@ namespace P07_Functions
     {
         public static void Main(string[] args)
         {
+            Console.Clear(); // ekranı temizler
+
+            Console.WriteLine("******************************************");
+            Console.WriteLine("*                                        *");
+            Console.WriteLine("* STRING METOT/FONKSİYON ÖRNEKLERİ       *");
+            Console.WriteLine("*                                        *");
+            Console.WriteLine("******************************************\n\n\n");
+
+            #region Length Metodu
+            // Verilen metin bilgisinin kac karakter olduğunu söyler..Integer türünden bir veri döndürür
+
+            string metin1 = "MCSD C# Dersleri";
+
+            int sonuc1 = metin1.Length;
+
+            Console.WriteLine($"Karakter adedi {sonuc1}");
+
+
+            #endregion
+
+            #region ToLower, ToUpper
+            // verilen bir metni küçük harften büyüğe, büyük haften küçüğe çevirmek için
+
+            string metin2 = "mcsd c# dersleri";
+            string metin3 = "MCSD C# DERSLERİ";
+
+            string ymetin2 = metin2.ToUpper();
+            string ymetin3 = metin3.ToLower();
+
+            Console.WriteLine($"Eski metin2 {metin2}  Yeni durumu {ymetin2}\n\n");
+            Console.WriteLine($"Eski metin3 {metin3}  Yeni durumu {ymetin3}\n\n");
 
 
 
+
+
+            #endregion
 
         }
 
