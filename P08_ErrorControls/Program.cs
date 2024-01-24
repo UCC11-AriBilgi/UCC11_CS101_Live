cd3dcf832e34c0f580227fd2ca4b0b6be94207a8
ukaracivio <umit.karacivi@outlook.com>
2024-01-24 21:24:55 +0300

    Some changes

--- P08_ErrorControls/Program.cs
+++ P08_ErrorControls/Program.cs
@@ -11,36 +11,53 @@
         #endregion
 
         #region Hata Avcısı
-        int c = 0;
-        int d = 0;
+        //int c = 0;
+        //int d = 0;
+
+        //try
+        //{
+        //    Console.WriteLine($" c / d işleminin sonuc {c / d} dir...");
+        //}
+        //catch (Exception)
+        //{
+        //    if (c!=0 && d==0)
+        //    {
+        //        d = c / 10;
+
+        //        Console.WriteLine($"d nin değeri hata oluşturduğu için güncellenmiş ve {d} yapılmıştır...Şimdi sonuc {c/d} dir..."  );
+        //    }
+        //    else if (c==0 && d != 0)
+        //    {
+        //        c = d * 10;
+
+        //        Console.WriteLine($"c nin değeri sonucu 0 vereceği için  için güncellenmiş ve {c} yapılmıştır...Şimdi sonuc {c / d} dir...");
+        //    }
+        //    else if (c == 0 && d == 0)
+        //    {
+        //        c = 50;
+        //        d = 10;
+
+        //        Console.WriteLine($"Şimdi sonuc {c / d} dir...");
+        //    }
+        //}
+        #endregion
+
+        #region Hata Metotları
+        byte deger;
 
         try
         {
-            Console.WriteLine($" c / d işleminin sonuc {c / d} dir...");
+            Console.WriteLine("Byte türünden bir değer giriniz :");
+            
+            deger=Convert.ToByte( Console.ReadLine() );
+
         }
-        catch (Exception)
+        catch (Exception e)
         {
-            if (c!=0 && d==0)
-            {
-                d = c / 10;
+            Console.WriteLine($"Message : {e.Message}\n\nSource : {e.Source}\n\nStackTrace : {e.StackTrace}\n\nTargetSite : {e.TargetSite}");
 
-                Console.WriteLine($"d nin değeri hata oluşturduğu için güncellenmiş ve {d} yapılmıştır...Şimdi sonuc {c/d} dir..."  );
-            }
-            else if (c==0 && d != 0)
-            {
-                c = d * 10;
 
-                Console.WriteLine($"c nin değeri sonucu 0 vereceği için  için güncellenmiş ve {c} yapılmıştır...Şimdi sonuc {c / d} dir...");
-            }
-            else if (c == 0 && d == 0)
-            {
-                c = 50;
-                d = 10;
-
-                Console.WriteLine($"Şimdi sonuc {c / d} dir...");
-            }
         }
-
 
 
 
